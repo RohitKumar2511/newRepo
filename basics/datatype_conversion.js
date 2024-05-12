37b88c5fae3ab257b427dcb4f28da9eec0006cac
Rohit Kumar Mahato <55598328+RohitKumar2511@users.noreply.github.com>
2024-05-12 20:19:54 +0000

    typeconversions

---
+++ basics/datatype_conversion.js
@@ -0,0 +1,33 @@
+/*                                  // number 
+let vrb = "rohit"
+
+console.log(typeof vrb);
+console.log(typeof(vrb));
+
+// let inf = 'true';
+// let ValInNum = Number(inf)
+// console.log("Value in number: "+ ValInNum);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+let score = true
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+*/
+
+
+/*                      // boolean
+let isLogg = "1"
+let booleanIsLoggedIn = Boolean(isLogg)
+console.log(booleanIsLoggedIn);
+
+// Number =>>  1 => true; 0 => false
+// "" => false // 
+//  "0" / "hitesh" => true >> it should not be empty
+*/
+
+let val = 33
+let strVal = String(val)
+console.log(strVal);
